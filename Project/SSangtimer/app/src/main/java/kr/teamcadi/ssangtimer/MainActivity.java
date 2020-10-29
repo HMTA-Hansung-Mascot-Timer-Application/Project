4ef5f8967fff00c1a0560ae523519a6d2e03a4e3
kyu9 <kyudo97@naver.com>
2020-10-29 14:24:31 +0900

    gif add

--- Project/SSangtimer/app/src/main/java/kr/teamcadi/ssangtimer/MainActivity.java
+++ Project/SSangtimer/app/src/main/java/kr/teamcadi/ssangtimer/MainActivity.java
@@ -6,54 +6,64 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+
+public class MainActivity extends AppCompatActivity  {
     private Button btn_start,btn_info,btn_setting,btn_enroll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btn_start = (Button) findViewById(R.id.btn_start);
         btn_enroll = (Button) findViewById(R.id.btn_confirm);
         btn_info = (Button) findViewById(R.id.btn_info);
         btn_setting = (Button) findViewById(R.id.btn_setting);
 
-        btn_start.setOnClickListener(this);
-        btn_enroll.setOnClickListener(this);
-        btn_info.setOnClickListener(this);
-        btn_setting.setOnClickListener(this);
+        ImageView reading = (ImageView) findViewById(R.id.reading);
+        GlideDrawableImageViewTarget gifimage = new GlideDrawableImageViewTarget(reading);
 
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        switch (id){
-            case R.id.btn_start:{
-                Intent intent1 = new Intent(MainActivity.this,StartActivity.class);
-                startActivityForResult(intent1,1);
-                break;
-            }
-
-            case R.id.btn_setting:{
-                Intent intent1 = new Intent(MainActivity.this,SettingActivity.class);
-                startActivityForResult(intent1,2);
-                break;
-            }
-
-            case R.id.btn_confirm:{
-                Intent intent1 = new Intent(MainActivity.this,EnrollActivitiy.class);
-                startActivityForResult(intent1,3);
-                break;
-            }
-
-            case R.id.btn_info:{
-                Intent intent1 = new Intent(MainActivity.this,InfoActivity.class);
-                startActivityForResult(intent1,4);
-                break;
-            }
-        }
+//        Glide.with(this).load(R.drawable.reading).into(gifimage);
+////
+////        btn_start.setOnClickListener(this);
+////        btn_enroll.setOnClickListener(this);
+////        btn_info.setOnClickListener(this);
+////        btn_setting.setOnClickListener(this);
+////
+////    }
+////
+////    @Override
+////    public void onClick(View v) {
+////        int id = v.getId();
+////        switch (id){
+////            case R.id.btn_start:{
+////                Intent intent1 = new Intent(MainActivity.this,StartActivity.class);
+////                startActivityForResult(intent1,1);
+////                break;
+////            }
+////
+////            case R.id.btn_setting:{
+////                Intent intent1 = new Intent(MainActivity.this,SettingActivity.class);
+////                startActivityForResult(intent1,2);
+////                break;
+////            }
+////
+//            case R.id.btn_confirm:{
+//                Intent intent1 = new Intent(MainActivity.this,EnrollActivitiy.class);
+//                startActivityForResult(intent1,3);
+//                break;
+//            }
+//
+//            case R.id.btn_info:{
+//                Intent intent1 = new Intent(MainActivity.this,InfoActivity.class);
+//                startActivityForResult(intent1,4);
+//                break;
+//            }
+//        }
 
     }
 }
--- Project/SSangtimer/app/src/main/java/kr/teamcadi/ssangtimer/StartActivity.java
+++ Project/SSangtimer/app/src/main/java/kr/teamcadi/ssangtimer/StartActivity.java
@@ -6,7 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 
 public class StartActivity extends AppCompatActivity {
     private Button btn_confirm;
@@ -14,6 +18,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
         btn_confirm =(Button)findViewById(R.id.btn_confirm);
         btn_confirm.setOnClickListener(new View.OnClickListener() {
             @Override
